df6f7377e158bcf80d90e9344a4d0818366a485b
uiliukhina <uilluhina@icloud.com>
2024-03-13 20:55:30 +0300

    Observability: Grafana, Prometheus, Loki, Promtail (Practical 10)

--- hotel-catalog-service/app/endpoints/hotel_catalog_router.py
+++ hotel-catalog-service/app/endpoints/hotel_catalog_router.py
@@ -1,18 +1,52 @@
 import uuid
 from uuid import UUID
-from fastapi import APIRouter, Depends, HTTPException, Body
+from fastapi import APIRouter, Depends, HTTPException, Body, Response
 
 from app.models.hotel import Hotel, CreateHotelRequest
 # from app.rabbitmq import send_booking
 from app.services.hotel_catalog_service import HotelCatalogService
 
+import prometheus_client
+
 hotel_catalog_router = APIRouter(prefix='/hotel-catalog', tags=['HotelCatalog'])
+
+metrics_router = APIRouter(tags=['Metrics'])
+
+get_hotels_count = prometheus_client.Counter(
+    "get_hotels_count",
+    "Number of get requests for hotels"
+)
+
+get_hotel_count = prometheus_client.Counter(
+    "get_hotel_count",
+    "Number of get requests for hotel by id"
+)
+
+add_hotel_count = prometheus_client.Counter(
+    "add_hotel_count",
+    "Number of created hotels"
+)
+update_hotel_count = prometheus_client.Counter(
+    "update_hotel_count",
+    "Number of updated hotels"
+)
+
+delete_hotel_count = prometheus_client.Counter(
+    "delete_hotel_count",
+    "Number of deleted hotels"
+)
+
+failed_get_hotel_count = prometheus_client.Counter(
+    "failed_get_hotel_count",
+    "Number of failed attempts to get hotel by id"
+)
 
 name = 'HotelCatalog with Booking Service'
 
 
 @hotel_catalog_router.get('/')
 def get_hotels(hotel_catalog_service: HotelCatalogService = Depends(HotelCatalogService)) -> list[Hotel]:
+    get_hotels_count.inc(1)
     return hotel_catalog_service.get_hotels()
 
 
@@ -26,8 +60,10 @@ def get_hotels(hotel_catalog_service: HotelCatalogService = Depends(HotelCatalog
 def get_hotel(hotel_id: UUID, hotel_catalog_service: HotelCatalogService = Depends(HotelCatalogService)) -> Hotel:
     hotel = hotel_catalog_service.get_hotel_by_id(hotel_id)
     if hotel:
+        get_hotels_count.inc(1)
         return hotel.dict()
     else:
+        failed_get_hotel_count.inc(1)
         raise HTTPException(404, f'Hotelwith id={hotel_id} not found')
 
 
@@ -43,7 +79,7 @@ def add_hotel(request: CreateHotelRequest,
     user_id = uuid.uuid4()
 
     # await send_booking(user_id, new_hotel.id)
-
+    add_hotel_count.inc(1)
     return new_hotel.dict()
 
 
@@ -52,10 +88,21 @@ def update_hotel(hotel_id: UUID, request: CreateHotelRequest,
                  hotel_catalog_service: HotelCatalogService = Depends(HotelCatalogService)) -> Hotel:
     updated_hotel = hotel_catalog_service.update_hotel(hotel_id, request.name, request.image_url, request.city,
                                                        request.description)
+
+    update_hotel_count.inc(1)
     return updated_hotel.dict()
 
 
 @hotel_catalog_router.delete('/delete/{hotel_id}')
 def delete_hotel(hotel_id: UUID, hotel_catalog_service: HotelCatalogService = Depends(HotelCatalogService)) -> None:
     hotel_catalog_service.delete_hotel(hotel_id)
+    delete_hotel_count.inc(1)
     return {'message': 'Hotel deleted successfully'}
+
+
+@metrics_router.get('/metrics')
+def get_metrics():
+    return Response(
+        media_type="text/plain",
+        content=prometheus_client.generate_latest()
+    )
--- hotel-catalog-service/app/main.py
+++ hotel-catalog-service/app/main.py
@@ -3,20 +3,28 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import asyncio
+import asyncio
 from fastapi import FastAPI
+from logging_loki import LokiHandler
+import logging
+
 
 from app.settings import settings
 
-from app.endpoints.hotel_catalog_router import hotel_catalog_router
+from app.endpoints.hotel_catalog_router import hotel_catalog_router, metrics_router
 
 name = 'HotelCatalog with Bookings Service'
 
 app = FastAPI(title=name)
 
-# @app.on_event('startup')
-# def startup():
-#     loop = asyncio.get_event_loop()
-#     asyncio.ensure_future(rabbitmq.consume(loop))
+loki_logs_handler = LokiHandler(
+    url="http://loki:3100/loki/api/v1/push",
+    tags={"application": "fastapi"},
+    version="1",
+)
 
+logger = logging.getLogger("uvicorn.access")
+logger.addHandler(loki_logs_handler)
 
 app.include_router(hotel_catalog_router, prefix='/hotel-api')
+app.include_router(metrics_router)
